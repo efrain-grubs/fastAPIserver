ffcc44157b72961b80d15301838f5c9224d98e99
Efrain <exa230030@utdallas.edu>
2025-12-01 14:57:12 -0600

    hi

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 from fastapi import FastAPI
-from inference_sdk import InferenceHTTPClient
+
 import os
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    CLIENT = InferenceHTTPClient(
-    api_url="https://serverless.roboflow.com",
-    api_key="ZFtUgbaxUbTvna51Ypc0"
-    )
 
-    #hello
-
-    baseURL = 'DataImages/'
-    # result = CLIENT.infer(baseURL+'cat.jpeg', model_id="sandwich-tqrld/1")
-    # result = CLIENT.infer(baseURL+'hamSandwich.jpg', model_id="sandwich-tqrld/1")
-    result = CLIENT.infer(baseURL+'hotdog.jpeg', model_id="sandwich-tqrld/1")
-    # result = CLIENT.infer(baseURL+'dog.jpeg', model_id="sandwich-tqrld/1")
-    #hi there
-    # Check if any predictions were made
-    if result['predictions'] and len(result['predictions']) > 0:
-        print("THIS IS A SANDWICH")
-
-        return "THIS IS A SANDWICH"
-    else:
-        print("THIS ISN'T A SANDWICH")
-        return "THIS ISN'T A SANDWICH"
+    
+    return {"Hello": "World"}
+    
 
    
 
